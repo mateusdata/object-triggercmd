5f8ac0732858bb863bbdabc6b94cd4a4570f8eed
Mateus Santos <54743403+mateusdata@users.noreply.github.com>
2022-08-29 01:29:20 -0300

    Create trigger.js

---
+++ trigger.js
@@ -0,0 +1,146 @@
+[
+ {
+  "trigger": "suspender",
+  "command": "systemctl suspend",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "suspender",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "abrir google chrome",
+  "command": "/usr/bin/google-chrome-stable %U",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "abrir chrome",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "fechar google chrome",
+  "command": "pkill chrome",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "fechar chrome",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "fechar vs code",
+  "command": "pkill code",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "fechar vscode",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "abrir vs code",
+  "command": "/usr/share/code/code --unity-launch",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "abrir vscode",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "atualizar o linux",
+  "command": "apt update",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "atualizar o linux",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "abrir terminal",
+  "command": "exo-open --launch TerminalEmulator",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "janela preta",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "fechar terminal",
+  "command": "pkill terminal",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "janela preta fechada",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "desligar o linux",
+  "command": "shutdown now",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "poweroff",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "ligar bluetooth",
+  "command": "service bluetooth start",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "ligar bluetooh do pc",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "reniciar o linux",
+  "command": "reboot",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "reiniciar linux",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "waths",
+  "command": "/opt/google/chrome/google-chrome --profile-directory=Default --app-id=hnpfjngllnobngcgfapefoaidbinmjnm",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "wathapp abrir",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "tiringa",
+  "command": "/opt/google/chrome/google-chrome --profile-directory=Default --app-id=agimnkijcaahngcdmfeangaknmldooml \"--app-launch-url-for-shortcuts-menu-item=https://www.youtube.com/watch?v=lHdHWfDSYps&list=PLSBgaYsUwfq-2sWuhf3OTxnXIFgzB-QV-\"",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "tir",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "instagran",
+  "command": "/opt/google/chrome/google-chrome --profile-directory=Default --app-id=maonlnecdeecdljpahhnnlmhbmalehlm",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "ins",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "github",
+  "command": "/opt/google/chrome/google-chrome --profile-directory=Default --app-id=mjoklplbddabcmpepnokjaffbmgbkkgg",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "git",
+  "voiceReply": "",
+  "allowParams": "false"
+ },
+ {
+  "trigger": "som do povo",
+  "command": "/opt/google/chrome/google-chrome --profile-directory=Default --app-id=agimnkijcaahngcdmfeangaknmldooml \"--app-launch-url-for-shortcuts-menu-item=https://www.youtube.com/watch?v=3TMsJDSbH2s&t=191s\"",
+  "offCommand": "",
+  "ground": "foreground",
+  "voice": "som do povo",
+  "voiceReply": "",
+  "allowParams": "false"
+ }
+]
